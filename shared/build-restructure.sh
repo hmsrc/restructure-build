58efaca961882fa105fd2ea85256c455bb3f0fec
Phil <phil.ayres@consected.com>
2022-10-26 15:09:59 +0100

    Fixed failing build

--- shared/build-restructure.sh
+++ shared/build-restructure.sh
@@ -128,6 +128,9 @@ if [ "${PROD_REPO_URL}" ]; then
   git remote set-url --push --add origin ${PROD_REPO_URL}
   git remote set-url --delete origin ${REPO_URL}
   git pull
+  git checkout new-master
+  git pull
+  git checkout develop
   git merge origin/${BUILD_GIT_BRANCH} -m "Merge remote" &&
     git commit -a -m "Commit"
   git push -f
